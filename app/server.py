03c86becccb3236632cb2a4fda6dbd724d6a7cb1
DJBRAMALL <39333283+DJBRAMALL@users.noreply.github.com>
2019-06-24 07:42:11 +0100

    Update server.py

--- app/server.py
+++ app/server.py
@@ -9,10 +9,10 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
+export_file_url = 'https://drive.google.com/uc?authuser=0&id=1KovG3fhL6RW7GyarbSOBHtzVDSsrBkU9&export=download'
 export_file_name = 'export.pkl'
 
-classes = ['black', 'grizzly', 'teddys']
+classes = ['AloeVera','Antirrhinum','Aubretia','Bindweed','Bramble','Cactus','Campanula','Chickweed','Clover','Dandelion']
 path = Path(__file__).parent
 
 app = Starlette()
